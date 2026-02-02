7c644ad7c54b48fb7f64ffa4ac5d2691d7fe4d25
Abhi singh <himanshukumar68327@gmail.com>
2026-02-02 21:53:23 +0530

    Add Google verification file

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1361827567"
+    serviceWorkerVersion: "2850073306"
   }
 });
